8e92578609be8c120c9184bc546eef7405153d91
William <530413@stud.hvl.no>
2022-06-20 13:21:44 +0200

    Første

---
+++ Test-1.py
@@ -0,0 +1,27 @@
+# Print "Hallo World"
+print("Hallo World")
+
+# Print "Hallo World" with a new line
+print("Hallo World\n")
+
+# Print "Hallo World" with a new line and a tab
+print("Hallo World\n\t")
+
+# Print "Hallo World" with a new line and a tab and a space
+print("Hallo World\n\t ")
+
+# Print "Hallo World" with a new line and a tab and a space and a number
+print("Hallo World\n\t ", 1)
+
+# Print "Hallo World" with a new line and a tab and a space and a number and a string
+print("Hallo World\n\t ", 1, "Hallo World")
+
+# Print "Hallo World" with a new line and a tab and a space and a number and a string and a boolean
+print("Hallo World\n\t ", 1, "Hallo World", True)
+
+# Print "Hallo World" with a new line and a tab and a space and a number and a string and a boolean and a list
+print("Hallo World\n\t ", 1, "Hallo World", True, [1, 2, 3])
+
+#Input for user
+name = input("Enter your name: ")
+print("Hello", name)
